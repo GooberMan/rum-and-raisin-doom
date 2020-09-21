fda8ef1484d80c6381b3e34937607c73de2cd2f0
Ethan Watson <gooberman@gmail.com>
2020-09-22 02:36:50 +0300

    Status bar turned off. It breaks things.

--- src/doom/st_stuff.c
+++ src/doom/st_stuff.c
@@ -1087,10 +1087,12 @@ void ST_Drawer (boolean fullscreen, boolean refresh)
     // Do red-/gold-shifts from damage/items
     ST_doPaletteStuff();
 
+#if 0
     // If just after ST_Start(), refresh all
     if (st_firsttime) ST_doRefresh();
     // Otherwise, update as little as possible
     else ST_diffDraw();
+#endif
 
 }
 
