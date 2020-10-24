d23d1ed525978453ef841fcd29323d15e64fdedc
Ethan Watson <gooberman@gmail.com>
2020-10-25 00:13:45 +0300

    Unsigned version of endian swap. Think I might make it better still.

--- src/i_swap.h
+++ src/i_swap.h
@@ -33,6 +33,9 @@
 #define SHORT(x)  ((signed short) SDL_SwapLE16(x))
 #define LONG(x)   ((signed int) SDL_SwapLE32(x))
 
+#define USHORT(x)  ((uint16_t) SDL_SwapLE16(x))
+#define ULONG(x)   ((uint32_t) SDL_SwapLE32(x))
+
 // Defines for checking the endianness of the system.
 
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
