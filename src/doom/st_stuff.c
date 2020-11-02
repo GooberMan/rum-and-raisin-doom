77b2b69a6f33a605d0b854fb05e42f79af432dea
Ethan Watson <gooberman@gmail.com>
2020-11-02 22:15:22 +0200

    Clang is rightfully warning on this one, now it uses the correct flags

--- src/doom/st_stuff.c
+++ src/doom/st_stuff.c
@@ -679,7 +679,7 @@ ST_Responder (event_t* ev)
 
       // So be it.
       plyr->message = DEH_String(STSTR_CLEV);
-      G_DeferedInitNew(gameskill, epsd, map, false);
+      G_DeferedInitNew(gameskill, epsd, map, GF_None);
     }
   }
   return false;
