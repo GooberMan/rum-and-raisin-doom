79ec0a9db17645b8b4e86c8360918e564936e657
Ethan Watson <gooberman@gmail.com>
2022-05-28 22:02:20 +0300

    I just spent hours upgrading to MSVC2022 because I wanted C++20 support, so this is going in to my git now.
    Future work will involve overhauling the Chocco build chain. Think I'll keep autoconf for Linux, but unsure about OSX.
    
    Also future work: Copying the required SDL/etc DLLs to the output folder.

---
+++ msvc/config.h
@@ -0,0 +1,11 @@
+#define PACKAGE_NAME "Rum and Raisin Doom"
+#define PACKAGE_TARNAME "run-and-raisin-doom"
+#define PACKAGE_VERSION "0.1.0"
+#define PACKAGE_STRING "Rum and Raisin Doom 0.1.0"
+#define PROGRAM_PREFIX "rum-and-raisin-"
+
+/* #undef HAVE_LIBSAMPLERATE */
+/* #undef HAVE_LIBPNG */
+/* #undef HAVE_DIRENT_H */
+#define HAVE_DECL_STRCASECMP 0
+#define HAVE_DECL_STRNCASECMP 0
