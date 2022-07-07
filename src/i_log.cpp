edf5d2f2cfc8fc03d8da6d10438cb181fa6e973e
Ethan Watson <gooberman@gmail.com>
2022-07-07 16:31:21 +0300

    Log rejigged to not conflict with new container types

--- src/i_log.cpp
+++ src/i_log.cpp
@@ -27,87 +27,10 @@ extern "C"
 
 #include <ctime>
 
-#include <vector>
-#include <string>
+#include "m_container.h"
 
-template< typename _ty, int32_t _tag = PU_STATIC >
-struct DoomAllocator
-{
-	enum : int32_t										{ tag = _tag };
-	typedef typename std::remove_cv< _ty >::type		value_type;
-	typedef value_type*									pointer;
-	typedef const pointer								const_pointer;
-	typedef value_type&									reference;
-	typedef const value_type&							const_reference;
-	typedef size_t										size_type;
-	typedef ptrdiff_t									difference_type;
-	typedef std::true_type								propagate_on_container_move_assignment;
-	template< typename _newty >
-	struct rebind
-	{
-		typedef DoomAllocator< _newty, tag >			other;
-	};
-	typedef std::true_type								is_always_equal;
-
-	DoomAllocator() = default;
-	~DoomAllocator() = default;
-
-	pointer address( reference x ) noexcept
-	{
-		return &x;
-	}
-
-	const_pointer address( const_reference x ) const noexcept
-	{
-		return &x;
-	}
-
-	pointer allocate( size_type n, const void * hint = 0 )
-	{
-		return ( pointer )Z_Malloc( sizeof( value_type ) * n, tag, NULL );
-	}
-
-	void deallocate( pointer p, std::size_t n )
-	{
-		Z_Free( p );
-	}
-
-	size_type max_size() const noexcept
-	{
-		return std::numeric_limits<size_type>::max() / sizeof(value_type);
-	}
-
-	template< class U, class... Args >
-	void construct( U* p, Args&&... args )
-	{
-		::new( (void *)p ) U( std::forward< Args >( args )... );
-	}
-
-	template< class U >
-	void destroy( U* p )
-	{
-		p->~U();
-	}
-
-	template< class _other >
-	bool operator==( const DoomAllocator< _other >& rhs ) noexcept
-	{
-		return std::is_same< value_type, _other >::value;
-	}
-
-	template< class _other >
-	bool operator!=( const DoomAllocator< _other >& rhs ) noexcept
-	{
-		return !std::is_same< value_type, _other >::value;
-	}
-};
-
-// Can't use our zone allocator just yet, because we try to log messages before Z_Init
-//template< typename _ty, int32_t _tag = PU_STATIC > using DoomVector = std::vector< _ty, DoomAllocator< _ty, _tag > >;
-//using DoomString = std::basic_string< char, std::char_traits< char >, DoomAllocator< char, PU_STATIC > >;
-
-template< typename _ty > using DoomVector = std::vector< _ty >;
-using DoomString = std::string;
+template< typename _ty > using LogVector = std::vector< _ty >;
+using LogString = std::string;
 
 typedef struct logentry_s
 {
@@ -129,11 +52,11 @@ typedef struct logentry_s
 
 	std::time_t				timestamp;
 	int32_t					type;
-	DoomString				message;
-	DoomString				timestring;
+	LogString				message;
+	LogString				timestring;
 } logentry_t;
 
-static DoomVector< logentry_t >	logentries;
+static LogVector< logentry_t >	logentries;
 
 extern "C" {
 
