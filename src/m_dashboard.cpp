154122f661fb0fe99345badf6ba640fb4cc2c7db
Ethan Watson <gooberman@gmail.com>
2022-07-07 19:29:49 +0300

    Adding SDL licence to the licences window

--- src/m_dashboard.cpp
+++ src/m_dashboard.cpp
@@ -449,6 +449,27 @@ typedef struct licenses_s
 
 static licenses_t licences[] =
 {
+	{ "Simple DirectMedia Layer",
+		"zlib license\n"
+		"\n"
+		"Copyright (C) 1997-2020 Sam Lantinga <slouken@libsdl.org>\n"
+  		"\n"
+		"This software is provided 'as-is', without any express or implied\n"
+		"warranty.  In no event will the authors be held liable for any damages\n"
+		"arising from the use of this software.\n"
+		"\n"
+		"Permission is granted to anyone to use this software for any purpose,\n"
+		"including commercial applications, and to alter it and redistribute it\n"
+		"freely, subject to the following restrictions:\n"
+  		"\n"
+		"1. The origin of this software must not be misrepresented; you must not\n"
+		"   claim that you wrote the original software. If you use this software\n"
+		"   in a product, an acknowledgment in the product documentation would be\n"
+		"   appreciated but is not required. \n"
+		"2. Altered source versions must be plainly marked as such, and must not be\n"
+		"   misrepresented as being the original software.\n"
+		"3. This notice may not be removed or altered from any source distribution.\n"
+	},
 	{ "Dear ImGui",
 		"The MIT License (MIT)\n"
 		"\n"
