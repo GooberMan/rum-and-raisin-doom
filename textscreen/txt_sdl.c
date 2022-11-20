5a6d5cb68fb6509e76403ade09fcf09004771642
Ethan Watson <gooberman@gmail.com>
2022-11-20 02:55:34 +0200

    Removing redefinition of macro

--- textscreen/txt_sdl.c
+++ textscreen/txt_sdl.c
@@ -108,7 +108,9 @@ static const SDL_Color ega_colors[] =
 
 #ifdef _WIN32
 
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif // WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
 // Examine system DPI settings to determine whether to use the large font.
