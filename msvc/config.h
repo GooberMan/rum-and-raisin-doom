c01ce01401e7de9c79c0d26d8a3cb0b7410ac4c5
Ethan Watson <gooberman@gmail.com>
2022-11-28 00:55:02 +0200

    Version bumps

--- msvc/config.h
+++ msvc/config.h
@@ -1,9 +1,9 @@
 #define PACKAGE_NAME "Rum and Raisin Doom"
 #define PACKAGE_TARNAME "run-and-raisin-doom"
-#define PACKAGE_VERSION "0.3.1-pre.3"
-#define PACKAGE_STRING "Rum and Raisin Doom 0.3.1-pre.3"
+#define PACKAGE_VERSION "0.3.1-pre.4"
+#define PACKAGE_STRING "Rum and Raisin Doom 0.3.1-pre.4"
 #define PROGRAM_PREFIX "rum-and-raisin-"
-#define PACKAGE_BANNER_BACK TXT_COLOR_RED
+#define PACKAGE_BANNER_BACK TXT_COLOR_CYAN
 
 #define HAVE_LIBPNG
 /* #undef HAVE_DIRENT_H */
