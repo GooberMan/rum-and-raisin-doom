05303293f245a792da896949bf34fbe0ac463504
Ethan Watson <gooberman@gmail.com>
2024-01-27 14:22:12 +0200

    pre.8

--- msvc/config.h
+++ msvc/config.h
@@ -1,7 +1,7 @@
 #define PACKAGE_NAME "Rum and Raisin Doom"
 #define PACKAGE_TARNAME "run-and-raisin-doom"
-#define PACKAGE_VERSION "0.3.1-pre.7"
-#define PACKAGE_STRING "Rum and Raisin Doom 0.3.1-pre.7"
+#define PACKAGE_VERSION "0.3.1-pre.8"
+#define PACKAGE_STRING "Rum and Raisin Doom 0.3.1-pre.8"
 #define PROGRAM_PREFIX "rum-and-raisin-"
 #define PACKAGE_BANNER_BACK TXT_COLOR_BROWN
 
